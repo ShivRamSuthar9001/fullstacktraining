b22253ed842f99c3c87ea1d52b5af264eca27359
Shiv Ram <shivrfd.suthar@gmail.com>
2020-11-23 19:04:19 +0530

    related to Burger_APP assignment

---
+++ assignment_solution/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+  state = {
+    username: 'Shiv Ram Suthar'
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="Saurabh" />
+      </div>
+    );
+  }
+}
+
+export default App;
